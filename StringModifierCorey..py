87294cc5cb509d5e222db7ec889e628693883fba
clodon2 <90060114+clodon2@users.noreply.github.com>
2022-05-25 11:47:17 -0400

    Add files via upload

---
+++ StringModifierCorey..py
@@ -0,0 +1,24 @@
+# Corey Verkouteren
+# 8/25/2021
+# Mr Ball's class
+# Using string methods
+
+# Transforms mixed case to upper
+MixedCase = "HeLlO"
+print(MixedCase)
+print(MixedCase.upper())
+
+# Indicates if a string is all numbers using true or false
+Alphanumeric = "jk1j"
+print(Alphanumeric)
+print(Alphanumeric.isnumeric())
+
+# Inverts the case of the string
+RandomCase = "AlpHaBEt"
+print(RandomCase)
+print(RandomCase.swapcase())
+
+# Capitalizes the first letter of each words
+ImproperGrammar = "corey verkouteren"
+print(ImproperGrammar)
+print(ImproperGrammar.title())
